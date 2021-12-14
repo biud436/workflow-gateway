5fcdef93613c348f2ca1205121959d679107548d
biud436 <biud436@gmail.com>
2021-12-14 11:47:37 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:alpine
+WORKDIR /usr/src/app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
+EXPOSE 3000
+ENV NODE_ENV production
+CMD [ "npm", "run" ,"start" ]
---
+++ src/app.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Get, HttpService, Post, Req } from '@nestjs/common';
+import * as nest from '@nestjs/common';
+import { AppService } from './app.service';
+import * as cp from 'child_process';
+import { Request } from 'express';
+import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private httpService: HttpService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/webhook')
+  async webhook(@Req() req: Request, @Body() body: any) {
+    const gateway =
+      cp.execSync("ip route | grep default | awk '{print $3}'") || '172.17.0.1';
+    const port = '3000';
+
+    const url = `http://${gateway}:${port}/webhook`;
+    const options = <AxiosRequestConfig>{
+      method: 'POST',
+      headers: req.headers,
+    };
+    const response = await this.httpService
+      .post(url, body, options)
+      .toPromise();
+    return await response.data;
+  }
+}
