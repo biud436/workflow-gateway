eb7d8bcabeba6bb91eecced80aa419bf07e462c5
biud436 <biud436@gmail.com>
2021-12-17 13:23:59 +0900

    added nest alias.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import * as nest from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await nest.NestFactory.create(AppModule);
   await app.listen(3001);
 }
 bootstrap();
